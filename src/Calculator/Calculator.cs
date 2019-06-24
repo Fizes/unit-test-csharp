d4dad62b677b29ba666d643792a455861e93f42d
Vyacheslav Karpov <vyacheslav.karpov@simbirsoft.com>
2019-06-24 14:38:29 +0400

    Add project files.

---
+++ src/Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        private long currentResult = 0;
+
+        public long Add(long value)
+        {
+            currentResult += value;
+            return currentResult;
+        }
+
+        public long Substract(long value)
+        {
+            currentResult -= value;
+            return currentResult;
+        }
+
+        public long Multiply(long value)
+        {
+            currentResult *= value;
+            return currentResult;
+        }
+
+        public long Divide(long value)
+        {
+            currentResult /= value;
+            return currentResult;
+        }
+    }
+}
---
+++ src/Calculator/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        private readonly Calculator calculatorProcessor;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            calculatorProcessor = new Calculator();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            var result = calculatorProcessor.Add(number);
+            tbx_Result.Text = result.ToString();
+        }
+
+        private void SubButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            var result = calculatorProcessor.Substract(number);
+            tbx_Result.Text = result.ToString();
+        }
+
+        private void MultButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            var result = calculatorProcessor.Multiply(number);
+            tbx_Result.Text = result.ToString();
+        }
+
+        private void DivButton_Click(object sender, EventArgs e)
+        {
+            var number = long.Parse(tbx_Number.Text);
+            var result = calculatorProcessor.Divide(number);
+            tbx_Result.Text = result.ToString();
+        }
+    }
+}
