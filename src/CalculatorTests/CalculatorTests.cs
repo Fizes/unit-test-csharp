570e159aeff6cb558d1ead799e55bb6dc6a931d9
ulskHeprit <xumuk495.x@ya.ru>
2019-07-01 19:27:49 +0400

    Tests added

---
+++ src/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            long temp = 999999999999999999;
+            long result;
+            Calculator calc = new Calculator();
+            result = calc.Add(temp);
+            Assert.AreEqual(temp, result);
+        }
+        
+        [TestMethod()]
+        public void SubstractTest1()
+        {
+            long temp = 999999999999999999;
+            long result;
+            Calculator calc = new Calculator();
+            result = calc.Substract(temp);
+            Assert.AreEqual(temp, result);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            long temp = 999999999999999999;
+            long result;
+            Calculator calc = new Calculator();
+            result = calc.Multiply(temp);
+            Assert.AreEqual(temp, result);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            long temp = 999999999999999999;
+            long result;
+            Calculator calc = new Calculator();
+            result = calc.Divide(temp);
+            Assert.AreEqual(temp, result);
+        }
+
+        [TestMethod()]
+        public void DivideTest1()
+        {
+            long temp = 0;
+            long result;
+            Calculator calc = new Calculator();
+            result = calc.Divide(temp);
+            Assert.AreEqual(temp, result);
+        }
+    }
+}
